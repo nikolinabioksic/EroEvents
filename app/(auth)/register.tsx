eddf41910025e6cd0f1acf71e18235fe599e385c
nikolinabioksic <nikolina.bioksic@fpmoz.sum.ba>
2026-06-05 17:18:16 +0200

    Postavljena navigacijska struktura za Sprint 1.

---
+++ app/(auth)/register.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function RegisterScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Ekran za registraciju (Ovdje kuca Član 3)</Text>
+    </View>
+  );
+}
